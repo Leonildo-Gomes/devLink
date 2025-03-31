a92fa4cdd1fbacc2023d5376bfee36199dbc9bb3
Leonildo-Gomes <leogomescv@gmail.com>
2025-03-31 23:16:59 +0200

    feat:  add Error component

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Admin } from './pages/admin';
+import { ErrorPage } from './pages/error';
 import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Networks } from './pages/networks';
@@ -12,6 +13,7 @@ import { Private } from './routes/Private';
                 { path: '/login', element: <Login/>},
                 { path: '/admin', element: <Private> <Admin/></Private>},
                 { path: '/admin/social', element: <Private> <Networks/> </Private>},
+                { path: '*', element: <ErrorPage/>}
             ],
         },
        
